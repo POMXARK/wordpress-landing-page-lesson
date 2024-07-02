aa468f6917864ca73705687b3d2f8a7496f87994
Роман Бушуев <bushuev@benefitty.ru>
2024-07-02 18:50:06 +0400

    feat[2]: add menu button sandwich
    Создание лендинга на WordPress. Часть 2. Parallax шапка
    45:00 - 1:08:00
    Добавлена анимированная кнопка для всплывающего открытия меню.

--- __clean_html_original/js/common.js
+++ __clean_html_original/js/common.js
@@ -6,6 +6,10 @@ $(window).resize(function() {
 	heightDetect();
 })
 
+$(".toggle_mnu").click(function() {
+	$(".sandwich").toggleClass("active");
+});
+
 $(window).load(function() {
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
