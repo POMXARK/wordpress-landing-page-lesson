2f890413115e7994dc2b87ba4427cca6a61fd98d
Роман Бушуев <bushuev@benefitty.ru>
2024-07-02 19:53:35 +0400

    feat[2]: add menu
    Создание лендинга на WordPress. Часть 2. Parallax шапка
    1:08:00 - 1:27:00
    Добавлено меню.

--- __clean_html_original/js/common.js
+++ __clean_html_original/js/common.js
@@ -10,6 +10,24 @@ $(".toggle_mnu").click(function() {
 	$(".sandwich").toggleClass("active");
 });
 
+$(".top_mnu ul a").click(function() {
+	$(".top_mnu").fadeOut(600);
+	$(".sandwich").toggleClass("active");
+	$(".top_text").css("opacity", "1");
+}).append("<span>");
+
+$(".toggle_mnu").click(function() {
+	if ($(".top_mnu").is(":visible")) {
+		$(".top_text").css("opacity", "1");
+		$(".top_mnu").fadeOut(600);
+		$(".top_mnu li a").removeClass("fadeInUp animated");
+	} else {
+		$(".top_text").css("opacity", ".1");
+		$(".top_mnu").fadeIn(600);
+		$(".top_mnu li a").addClass("fadeInUp animated");
+	};
+});
+
 $(window).load(function() {
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
