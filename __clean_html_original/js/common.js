cbe29983056c4c11ee7f6a3b678bd9de5c00067d
Роман Бушуев <bushuev@benefitty.ru>
2024-07-03 12:15:39 +0400

    feat[7]: add section resume, animate
    Создание лендинга на WordPress. Часть 7. Верстка второй секции

--- __clean_html_original/js/common.js
+++ __clean_html_original/js/common.js
@@ -36,6 +36,9 @@ $(".animation_1").animated("flipInY", "fadeOutDown");
 $(".animation_2").animated("fadeInLeft", "fadeOutDown");
 $(".animation_3").animated("fadeInRight", "fadeOutDown");
 
+$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
+
 $(window).load(function() {
 
 	$(".loader_inner").fadeOut();
