dc36d60db2a3b173ad100a0f9141117b1d2441f3
Роман Бушуев <bushuev@benefitty.ru>
2024-07-03 20:13:16 +0400

    feat[8]: add section portfolio, animate
    Создание лендинга на WordPress. Часть 8. Сортируемые плитки

--- __clean_html_original/js/common.js
+++ __clean_html_original/js/common.js
@@ -1,50 +1,71 @@
-$(".popup").magnificPopup({type:"image"});
+$(document).ready(function () {
 
-function heightDetect() {
-	$(".main_head").css("height", $(window).height());
-};
-heightDetect();
-$(window).resize(function() {
-	heightDetect();
-})
+    $("#portfolio_grid").mixItUp();
 
-$(".toggle_mnu").click(function() {
-	$(".sandwich").toggleClass("active");
+    $(".s_portfolio li").click(function () {
+        $(".s_portfolio li").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".popup").magnificPopup({type: "image"});
+    $(".popup_content").magnificPopup({
+        type:"inline",
+        midClick: true
+    });
+
+    function heightDetect() {
+        $(".main_head").css("height", $(window).height());
+    };
+    heightDetect();
+    $(window).resize(function () {
+        heightDetect();
+    })
+
+    $(".toggle_mnu").click(function () {
+        $(".sandwich").toggleClass("active");
+    });
+
+    $(".top_mnu ul a").click(function () {
+        $(".top_mnu").fadeOut(600);
+        $(".sandwich").toggleClass("active");
+        $(".top_text").css("opacity", "1");
+    }).append("<span>");
+
+    $(".toggle_mnu").click(function () {
+        if ($(".top_mnu").is(":visible")) {
+            $(".top_text").css("opacity", "1");
+            $(".top_mnu").fadeOut(600);
+            $(".top_mnu li a").removeClass("fadeInUp animated");
+        } else {
+            $(".top_text").css("opacity", ".1");
+            $(".top_mnu").fadeIn(600);
+            $(".top_mnu li a").addClass("fadeInUp animated");
+        }
+        ;
+    });
+
+    $(".section_header").animated("fadeInUp", "fadeOutDown");
+
+    $(".animation_1").animated("flipInY", "fadeOutDown");
+    $(".animation_2").animated("fadeInLeft", "fadeOutDown");
+    $(".animation_3").animated("fadeInRight", "fadeOutDown");
+
+    $(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+    $(".right .resume_item").animated("fadeInRight", "fadeOutDown");
+
+    $(".portfolio_item").each(function(i) {
+        $(this).find("a").attr("href", "#work_" + i);
+        $(this).find(".podrt_descr").attr("id", "work_" + i);
+    });
+
 });
 
-$(".top_mnu ul a").click(function() {
-	$(".top_mnu").fadeOut(600);
-	$(".sandwich").toggleClass("active");
-	$(".top_text").css("opacity", "1");
-}).append("<span>");
+$(window).load(function () {
 
-$(".toggle_mnu").click(function() {
-	if ($(".top_mnu").is(":visible")) {
-		$(".top_text").css("opacity", "1");
-		$(".top_mnu").fadeOut(600);
-		$(".top_mnu li a").removeClass("fadeInUp animated");
-	} else {
-		$(".top_text").css("opacity", ".1");
-		$(".top_mnu").fadeIn(600);
-		$(".top_mnu li a").addClass("fadeInUp animated");
-	};
-});
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
 
-$(".section_header").animated("fadeInUp", "fadeOutDown");
-
-$(".animation_1").animated("flipInY", "fadeOutDown");
-$(".animation_2").animated("fadeInLeft", "fadeOutDown");
-$(".animation_3").animated("fadeInRight", "fadeOutDown");
-
-$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
-$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
-
-$(window).load(function() {
-
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
-
-	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
-	$(".top_text p").animated("fadeInUp", "fadeOutDown");
+    $(".top_text h1").animated("fadeInDown", "fadeOutUp");
+    $(".top_text p").animated("fadeInUp", "fadeOutDown");
 
 });
