523dd0894e1337aee8c6cf8dff43f03baf7262c1
Роман Бушуев <bushuev@benefitty.ru>
2024-07-07 23:03:53 +0400

    feat: add header parallax
    Добавил header parallax.
    Объединил все скрипты.

--- gatsby-starter-wordpress-homepage/gatsby-browser.js
+++ gatsby-starter-wordpress-homepage/gatsby-browser.js
@@ -3,3 +3,26 @@ import "@fontsource/dm-sans/500.css"
 import "@fontsource/dm-sans/700.css"
 import "@fontsource/dm-mono"
 import "@fontsource/dm-mono/500.css"
+
+import * as React from "react";
+
+import "/libs/bootstrap/bootstrap-grid.min.css"
+import "/libs/font-awesome/css/font-awesome.min.css"
+import "/libs/linea/styles.css"
+import "/libs/magnific-popup/magnific-popup.css"
+import "/libs/animate/animate.min.css"
+import "/css/fonts.css"
+import "/css/main.css"
+import "/css/skins/purple.css"
+import "/css/media.css"
+
+//
+// const $ = require("jquery")
+// window.jQuery=$;
+// // import 'jquery-parallax.js'
+//
+// export const onInitialClientRender = () => {
+//     $(document).ready(function () {
+//         console.log("The answer is don't think about it!")
+//     });
+// }
--- gatsby-starter-wordpress-homepage/src/pages/index.js
+++ gatsby-starter-wordpress-homepage/src/pages/index.js
@@ -1,25 +1,44 @@
 import * as React from "react"
+// import styles from  "../../static/parallax.css"
 
-import AboutMy from "../components/modules/AboutMy";
-import Edu from "../components/modules/Edu";
-import Work from "../components/modules/Work";
+import ImgBg from '../img/bg.jpg'
+import {motion,useScroll, useTransform} from "framer-motion";
+import {useRef} from "react";
+import { UseScrollPosition } from 'react-hook-collections';
 import Header from "../components/modules/Header";
+import AboutMy from "../components/modules/AboutMy";
+import Work from "../components/modules/Work";
+import Edu from "../components/modules/Edu";
+
 
 export default function Homepage(props) {
 
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+      target: ref,
+      offset: ["start start", "end start"],
+  })
+    const backgroundY = useTransform(scrollYProgress, [0, 1],
+        ["0%", "100%"])
+    const textY = useTransform(scrollYProgress, [0, 1],
+        ["0%", "200%"])
+
+    const scrollPosition = UseScrollPosition();
+    console.log(scrollPosition); // Prints the scroll position on the console
+
   return (
     <>
-        {Header()}
-        <p>Обо мне</p>
-        {AboutMy()}
+      {Header()}
+      <p>Обо мне</p>
+      {AboutMy()}
 
-        <p>Резюме</p>
+      <p>Резюме</p>
 
-        <p>Работа</p>
-        {Work()}
+      <p>Работа</p>
+      {Work()}
 
-        <p>Учеба</p>
-        {Edu()}
+      <p>Учеба</p>
+      {Edu()}
     </>
   )
 }
